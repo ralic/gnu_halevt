623da981528464f2dfc3311a5af3382c96cff717
Patrice Dumas <pertusus@free.fr>
2010-02-14 21:42:42 +0100

    Adapt README to git

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,8 @@
 autopoint
 for file in po/*; do
   test $file = po/CVS && continue
+  # certainly not useful, the .git seems to be only in the toplevel directory
+  test $file = po/.git && continue
   test -f halevt-mount/$file || cp $file halevt-mount/$file
 done
 autoreconf -i
