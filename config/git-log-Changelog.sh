2cbead8a8c9bb8c200f9949720bdb68d7b4c5648
Patrice Dumas <pertusus@free.fr>
2010-02-18 12:31:32 +0100

    Use git-revert to add ChangeLog change

--- config/git-log-Changelog.sh
+++ config/git-log-Changelog.sh
@@ -1,12 +1,18 @@
 #! /bin/sh
 
-./config/gitlog-to-changelog -- '@{1}..' > ChangeLog-git.new
+#set -x
+./config/gitlog-to-changelog -- 'HEAD~1..HEAD' > ChangeLog-git.new
 echo >> ChangeLog-git.new
 cp ChangeLog ChangeLog-old.old || exit 1
 cat ChangeLog-git.new ChangeLog-old.old > ChangeLog || exit 1
 rm ChangeLog-old.old ChangeLog-git.new
-git commit -n --cleanup=verbatim -m '' ChangeLog
+git reset --soft HEAD^
+git add ChangeLog
+git commit -a -c ORIG_HEAD
+#git commit -n --cleanup=verbatim -m '' ChangeLog
 # in case there is uncommited changes
-git stash save --keep-index
-git rebase -i HEAD~2
-git stash pop
+# doesn't work... stash with and without keep-index reverts the ChangeLog...
+#git stash save 
+# without interactive rebase the empty commit message is left...
+#git rebase -i HEAD~2
+#git stash pop
