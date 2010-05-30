ralic/gnu_halevt

commit 72822c382d3ef426fc7ecf1104556066b5ff63f3

Resolve parents for exec properties, too

src/hal_interface.c
@@ -55,8 +55,9 @@ void halevt_check_dbus_error(DBusError *error)
 
 static char **halevt_duplicate_str_list(char** str_list)
 {
-   char **value = NULL;
+   char **value;
    char **cur_str;
+   int i;
 
    if (str_list != NULL)
    {
@@ -70,7 +71,14 @@ static char **halevt_duplicate_str_list(char** str_list)
        {
            return NULL;
        }
-       *value = NULL;
+
+       /* this should not be necessary, however one cannot count on 
+          libhal to do that. Besides, if the strdup below fails, this 
+          is preferable to have NULL values and not random ones */
+       for (i = 0; i < total_value_nr+1; i++)
+       {
+         *(value + i) = NULL;
+       }
 
        cur_value = value;
 
@@ -83,8 +91,10 @@ static char **halevt_duplicate_str_list(char** str_list)
                return NULL;
            }
            *cur_value = cur_val;
+           /* already done above
            cur_value++;
            *cur_value = NULL;
+           */
        }
    }
    return value;
@@ -184,43 +194,48 @@ static int halevt_property_matches_udi (const char *property,
    return result;
 }
 
-static int halevt_match_udi (const halevt_match *match, const char *udi)
+static char *halevt_property_resolve_udi(const halevt_property_name *property, const char *udi)
 {
     DBusError dbus_error;
     char **parent;
-    char *old_udi = strdup(udi);
-    int result = 0;
+    char *current_udi = strdup(udi);
 
     dbus_error_init(&dbus_error);
-
-    WALK_NULL_ARRAY(parent, match->parents)
+     
+    WALK_NULL_ARRAY(parent, property->parents)
     {
        char *new_udi;
 
-       if (old_udi == NULL) { goto out; }
+       if (current_udi == NULL) { return NULL; }
 
        if (! libhal_device_property_exists
-             (hal_ctx, old_udi, *parent, &dbus_error))
+             (hal_ctx, current_udi, *parent, &dbus_error))
        {
-          goto out;
+          return NULL;
        }
 
        halevt_check_dbus_error(&dbus_error);
 
        new_udi = libhal_device_get_property_string
-            (hal_ctx, old_udi, (*parent), &dbus_error);
-       free(old_udi);
+            (hal_ctx, current_udi, (*parent), &dbus_error);
+       free(current_udi);
        halevt_check_dbus_error(&dbus_error);
-       old_udi = new_udi;
+       current_udi = new_udi;
     }
+    return current_udi;
+}
 
-    if (old_udi != NULL)
+static int halevt_match_udi (const halevt_match *match, const char *udi)
+{
+    char *real_udi = halevt_property_resolve_udi(&(match->property), udi);
+    int result = 0;
+
+    if (real_udi != NULL)
     {
-        result = halevt_property_matches_udi(match->name, match->value, old_udi);
+        result = halevt_property_matches_udi(match->property.name, match->value, real_udi);
     }
 
-out:
-    free(old_udi);
+    free(real_udi);
     return result;
 }
 
@@ -252,28 +267,39 @@ static int halevt_property_matches_device (const char *key,
    return 0;
 }
 
-static int halevt_match_device (const halevt_match *match, const halevt_device *device)
+/*
+ * resolve the real device by going through the parents.
+ */
+static halevt_device *halevt_property_resolve_device(const halevt_property_name *property, const halevt_device *device)
 {
-    const halevt_device *new_device = device;
+    halevt_device *new_device = device;
     const halevt_device_property *udi_property;
     char **new_udi;
     char **parent;
-
-    WALK_NULL_ARRAY(parent, match->parents)
+    WALK_NULL_ARRAY(parent, property->parents)
     {
        udi_property = halevt_device_list_get_property (*parent, new_device);
        if (udi_property != NULL)
        {
           new_udi = udi_property->values;
           new_device = halevt_device_list_find_device(new_udi[0]);
-          if (new_device == NULL) { return 0 ;}
+          if (new_device == NULL) { return NULL ;}
        }
        else
        {
-          return 0;
+          return NULL;
        }
     }
-    return halevt_property_matches_device(match->name, match->value, new_device);
+    return new_device;
+}
+
+static int halevt_match_device (const halevt_match *match, const halevt_device *device)
+{
+    const halevt_device *real_device = halevt_property_resolve_device(&(match->property), device);
+    if (real_device == NULL)
+      return 0;
+
+    return halevt_property_matches_device(match->property.name, match->value, real_device);
 }
 
 /*
@@ -288,7 +314,7 @@ int halevt_matches (const halevt_match *match, const char *udi,
         return 0;
     }
 
-    if (!strcmp (match->name, "*")) { return 1; }
+    if (!strcmp (match->property.name, "*")) { return 1; }
 
     if (udi != NULL) { return halevt_match_udi(match, udi) ;}
     return (halevt_match_device (match, device));
@@ -297,9 +323,9 @@ int halevt_matches (const halevt_match *match, const char *udi,
 /*
  * return the value of a property for a device specified by its udi
  */
-static char **halevt_udi_property_value (const char *property, const char *udi)
+char **halevt_udi_property_value (const char *property, const char *udi)
 {
-    char **values = NULL;
+    char **values;
 
     DBusError dbus_error;
 
@@ -333,17 +359,18 @@ static char **halevt_udi_property_value (const char *property, const char *udi)
  * return the udi. Otherwise if a device is given look at the device
  * property, otherwise use hal to get the property.
  */
-char **halevt_property_value(const char *key, const char *udi,
-   const halevt_device* device)
+char **halevt_property_name_value(const halevt_property_name *property, 
+   const char *udi, const halevt_device* device)
 {
-    char **values;
+    char **values = NULL;
 
-    if ((udi == NULL) || (key == NULL))
+    if ((udi == NULL) || (property == NULL))
     {
-        DEBUG("Warning: halevt_property_value was called with a NULL value");
+        DEBUG("Warning: halevt_property_name_value was called with a NULL value");
         return NULL;
     }
-    if (!strcmp(key,"udi"))
+
+    if (!strcmp(property->name,"udi") && property->parents[0] == NULL)
     {
         char *new_udi = strdup(udi);
         if (new_udi == NULL) { return NULL; }
@@ -358,13 +385,22 @@ char **halevt_property_value(const char *key, const char *udi,
     }
     else if (device != NULL)
     {
-        halevt_device_property *property =
-             halevt_device_list_get_property(key, device);
-        values = ((property != NULL) ? halevt_duplicate_str_list(property->values) : NULL);
+        halevt_device *real_device = halevt_property_resolve_device(property, device);
+        if (real_device != NULL)
+        {
+           halevt_device_property *device_property = halevt_device_list_get_property(property->name, device);
+           if (device_property != NULL)
+              values = halevt_duplicate_str_list(device_property->values);
+        }
     }
     else
     {
-        values = halevt_udi_property_value(key, udi);
+        char *real_udi = halevt_property_resolve_udi(property, udi);
+        if (real_udi != NULL)
+        {
+          values = halevt_udi_property_value(property->name, real_udi);
+          free (real_udi);
+        }
     }
 
     return values;
@@ -387,6 +423,7 @@ static int halevt_run_command(const halevt_exec *exec, char const *udi,
     char **values;
     int str_len;
     int hal_value_used;
+    int i;
 
     if ((command = (char *) malloc (string_size*sizeof(char))) == NULL)
     {
@@ -394,15 +431,17 @@ static int halevt_run_command(const halevt_exec *exec, char const *udi,
     }
     *command = '\0';
 
-    while(exec->elements[string_index].string != NULL)
+    for (i = 0; i < exec->exec_size; i++)
     {
         hal_value_used = 0;
         if (exec->elements[string_index].hal_property)
         {
-            values = halevt_property_value (exec->elements[string_index].string, udi, device);
+            values = halevt_property_name_value (&(exec->elements[string_index].property), udi, device);
             if (values == NULL)
             {
-                DEBUG(_("Hal property %s in command '%s' not found (or OOM)"), exec->elements[string_index].string, exec->string);
+                char *property_string = halevt_print_property_name(&(exec->elements[string_index].property));
+                DEBUG(_("Hal property %s in command '%s' not found (or OOM)"), property_string, exec->string);
+                free(property_string);
                 string = "UNKNOWN";
             }
             else
@@ -422,12 +461,13 @@ static int halevt_run_command(const halevt_exec *exec, char const *udi,
             current_size += str_len;
             if ((command = (char *) realloc (command, string_size*sizeof(char))) == NULL)
             {
-                if (hal_value_used) { free (string); }
+                if (hal_value_used) 
+                  FREE_NULL_ARRAY(char *, values, free);
                 DEBUG(_("Out of memory, cannot run %s"), exec->string);
                 return 0;
             }
             strcat (command, string);
-            if (hal_value_used) { FREE_NULL_ARRAY(char *, values, free); } 
+            if (hal_value_used) { FREE_NULL_ARRAY(char *, values, free);}
         }
         string_index++;
     }
@@ -673,7 +713,7 @@ char **halevt_get_iterator_value(const LibHalPropertyType type,
 char **halevt_get_property_value(LibHalPropertyType type,
   const char *property, const char *udi, DBusError *dbus_error_pointer)
 {
-    char **value = NULL;
+    char **value;
     char tmp[256];
 
     if (type == LIBHAL_PROPERTY_TYPE_STRLIST)

src/devices_list.c
@@ -165,7 +165,7 @@ int halevt_device_list_set_property (const char *udi, const char *key)
 
    if (device == NULL) { return 0; }
 
-   values = halevt_property_value (key, udi, NULL);
+   values = halevt_udi_property_value (key, udi);
    if (values == NULL) { return 0; }
 
    property = halevt_device_list_get_property(key, device);

src/hal_interface.h
@@ -32,10 +32,11 @@ void halevt_cleanup_HAL();
 
 void halevt_check_dbus_error(DBusError *error);
 
+void halevt_run_oninit();
+
 int halevt_matches (const halevt_match* match, const char *udi,
    const halevt_device *device);
-char **halevt_property_value (const char* property, const char *udi,
-   const halevt_device* device);
+char **halevt_udi_property_value (const char* property, const char *udi);
 char **halevt_get_property_value(LibHalPropertyType type, const char *property,
    const char *udi, DBusError *dbus_error_pointer);
 char **halevt_get_iterator_value(const LibHalPropertyType type,

src/match.c
@@ -28,8 +28,6 @@
 #include "hal_interface.h"
 #include "parse_config.h"
 
-static const char *hal_substring = ".hal.";
-
 /* transform a string with boolean expression containing any character
  * to a string acceptable by boolstuff, containing only alphanumeric
  * characters. Here, all the atoms are substitued by a numeric index
@@ -160,77 +158,34 @@ halevt_boolean_expression *halevt_new_boolean_expression(const xmlChar *original
    }
    for (i=0; i < new_expression->matches_size; i++)
    {
+      char *match_property_string;
       halevt_match *new_match;
-      char *p;
-      char *string;
-      int parents_size = 0;
 
       new_match = &(new_expression->matches[i]);
 
       /* first parse a 'name = value' or a simple 'name', remove leading
-         sapces from value and trailing spaces from name */
+         spaces from value and trailing spaces from name */
       new_match->value = atoms[i];
 
-      new_match->name = strsep(&(new_match->value), "=");
+      match_property_string = strsep(&(new_match->value), "=");
       if (new_match->value != NULL)
       {
          while (isspace(*new_match->value)) { (new_match->value)++; }
-         s = strlen (new_match->name);
-         while (s > 0 && isspace (*(new_match->name + s-1))) { s--; };
-         new_match->name[s] = '\0';
+         s = strlen (match_property_string);
+         while (s > 0 && isspace (*(match_property_string + s-1))) { s--; };
+         match_property_string[s] = '\0';
       }
 
-      if ((halevt_hal_string(new_match->name)) == NULL)
+      if ((halevt_hal_string(match_property_string)) == NULL)
       {
-         DEBUG(_("Bad match expression: %s"), new_match->name);
-         goto free_match;
-      }
-      /* cut the string in substring separated by hal. the last
-         corresponds with the value, the other are the parents */
-
-      /* first find the number of parents */
-      p = new_match->name;
-      while ((p = strstr(p, hal_substring)) != NULL)
-      {
-         parents_size++;
-         p = p + 1;
-      }
-
-      if ((new_match->parents = malloc(sizeof(char *) * (parents_size + 1))) == NULL)
-      {
+         DEBUG(_("Bad match expression: %s"), match_property_string);
          goto free_match;
       }
 
-      if (parents_size > 0)
-      {
-         int parent_index = 0;
-         p = new_match->name;
-         new_match->parents[0] = new_match->name;
-         /* cut the string in pieces */
-         while ((p = strstr (p, hal_substring)) != NULL)
-         {
-            *p = '\0';
-            parent_index++;
-            new_match->parents[parent_index] = p+1;
-            p = p + 1;
-         }
+      halevt_parse_property_name(match_property_string, &(new_match->property));
 
-         for (j=0; j<parent_index+1; j++)
-         {
-            if ((string = halevt_hal_string(new_match->parents[j])) == NULL)
-            {
-               DEBUG(_("Bad match expression: %s"), new_match->parents[j]);
-               goto free_match;
-            }
-            new_match->parents[j] = string;
-         }
-         new_match->name = new_match->parents[parent_index];
-      }
-      else
-      {
-         new_match->name = halevt_hal_string(new_match->name);
-      }
-      new_match->parents[parents_size] = NULL;
+      if (new_match->property.name == NULL)
+         goto free_match;
    }
 
    /* now use boolstuff to parse the expression and transform to DNF */
@@ -275,7 +230,7 @@ void halevt_free_boolean_expression (halevt_boolean_expression *expr)
 
    for (i = 0; i < expr->matches_size; i++)
    {
-      free(expr->matches[i].parents);
+      free(expr->matches[i].property.parents);
    }
 
    boolstuff_destroy_tree (expr->tree);
@@ -285,39 +240,36 @@ void halevt_free_boolean_expression (halevt_boolean_expression *expr)
 
 char *halevt_print_boolean_expression (halevt_boolean_expression *expr)
 {
-   char *string;
+   char *string = NULL;
    char tmp[256];
    int total_size = 0;
-   int i, j;
+   int i;
+   char **match_properties_string;
+
+   if ((match_properties_string = (char **) malloc(expr->matches_size * sizeof(char *))) == NULL)
+      return NULL;
 
    for (i=0; i < expr->matches_size; i++)
    {
-      total_size += strlen(expr->matches[i].name) + 2;
-      if (expr->matches[i].value !=  NULL) { total_size += strlen(expr->matches[i].value) + 1; }
-      j=0;
-      while (expr->matches[i].parents[j] != NULL)
-      {
-         total_size += strlen(expr->matches[i].parents[j]) + 2;
-         j++;
-      }
+      match_properties_string[i] = halevt_print_property_name(&(expr->matches[i].property));
+      if (match_properties_string[i] != NULL)
+        total_size += strlen(match_properties_string[i]);
+      if (expr->matches[i].value !=  NULL) 
+        total_size += strlen(expr->matches[i].value) + 1;
+      total_size += 2;
    }
    sprintf(tmp, "%d: ", expr->matches_size);
    /* +2 for the first ", ", +1 for the end of line  */
    total_size += strlen(tmp) + strlen(expr->expression_string) +2 +1;
-   string = (char *) malloc(total_size * sizeof(char));
+   if ((string = (char *) malloc(total_size * sizeof(char))) == NULL)
+      goto oom;
    strcpy (string, tmp);
    strcat (string, expr->expression_string);
    strcat(string, ", ");
    for (i=0; i < expr->matches_size; i++)
    {
-      j=0;
-      while (expr->matches[i].parents[j] != NULL)
-      {
-         strcat(string, expr->matches[i].parents[j]);
-         strcat(string, "->");
-         j++;
-      }
-      strcat(string, expr->matches[i].name);
+      if (match_properties_string[i] != NULL)
+        strcat(string, match_properties_string[i]);
       if (expr->matches[i].value != NULL)
       {
          strcat(string, "=");
@@ -325,6 +277,12 @@ char *halevt_print_boolean_expression (halevt_boolean_expression *expr)
       }
       strcat(string, ", ");
    }
+oom:
+   for (i=0; i < expr->matches_size; i++)
+   {
+      free (match_properties_string[i]);
+   }
+   free (match_properties_string);
    return string;
 }
 

src/match.h
@@ -25,11 +25,16 @@
 #include <libxml/xmlstring.h>
 #include "devices_list.h"
 
-typedef struct halevt_match
+typedef struct halevt_property_name
 {
   char *name;
-  char *value;
   char **parents;
+} halevt_property_name;
+
+typedef struct halevt_match
+{
+  char *value;
+  halevt_property_name property;
 } halevt_match;
 
 typedef struct halevt_boolean_expression

src/parse_config.c
@@ -34,6 +34,7 @@ halevt_oninit *halevt_oninit_root = NULL;
 
 static const char *halevt_hal_prefix = "hal.";
 static const char *halevt_hal_exec_prefix = "$hal.";
+static const char *hal_substring = ".hal.";
 
 /*
  * returns the beginning of the string after the 'hal prefix', if
@@ -48,6 +49,64 @@ char *halevt_hal_string (char *string)
    return string + strlen(halevt_hal_prefix);
 }
 
+/* cut the string in substring separated by hal. the last
+   corresponds with the value, the other are the parents */
+
+void halevt_parse_property_name (char *string, halevt_property_name *property_name)
+{
+   char *p;
+   int parents_size = 0;   
+
+   property_name->name = string;
+
+   /* first find the number of parents */
+   p = string;
+   while ((p = strstr(p, hal_substring)) != NULL)
+   {
+      parents_size++;
+      p = p + 1;
+   }
+   if ((property_name->parents = malloc(sizeof(char *) * (parents_size + 1))) == NULL)
+   {
+     property_name->name = NULL;
+     return;
+   }
+   if (parents_size > 0)
+   {
+      int parent_index = 0;
+      int j;
+      char *string;
+      p = property_name->name;
+      property_name->parents[0] = property_name->name;
+      /* cut the string in pieces */
+      while ((p = strstr (p, hal_substring)) != NULL)
+      {
+         *p = '\0';
+         parent_index++;
+         property_name->parents[parent_index] = p+1;
+         p = p + 1;
+      }
+
+      for (j=0; j<parent_index+1; j++)
+      {
+        if ((string = halevt_hal_string(property_name->parents[j])) == NULL)
+        {
+            DEBUG(_("Bad match expression: %s"), property_name->parents[j]);
+            property_name->name = NULL;
+            free(property_name->parents);
+            return;
+        }
+        property_name->parents[j] = string;
+     }
+     property_name->name = property_name->parents[parent_index];
+   }
+   else
+   {
+      property_name->name = halevt_hal_string(property_name->name);
+   }
+   property_name->parents[parents_size] = NULL;
+}
+
 halevt_exec *halevt_new_exec(const xmlChar *exec)
 {
    char *string;
@@ -56,9 +115,8 @@ halevt_exec *halevt_new_exec(const xmlChar *exec)
    halevt_exec *result_exec;
    int sub_elements_number = 0;
    if ((string = (char *) xmlStrdup(exec)) == NULL)
-   {
       return NULL;
-   }
+
    /* handle the special case where there is a $hal. at the beginning */
    if (strlen(string) >= strlen(halevt_hal_exec_prefix) && !strncmp(halevt_hal_exec_prefix, string, strlen(halevt_hal_exec_prefix)))
    {
@@ -98,7 +156,7 @@ halevt_exec *halevt_new_exec(const xmlChar *exec)
       sub_elements_number++;
    }
    /* now allocate the array */
-   if ((result_exec->elements = (halevt_exec_element *) malloc(sizeof(halevt_exec_element) * (sub_elements_number +2)))  == NULL)
+   if ((result_exec->elements = (halevt_exec_element *) malloc(sizeof(halevt_exec_element) * (sub_elements_number +1)))  == NULL)
    {
       free (string);
       free (parsed_string);
@@ -110,6 +168,7 @@ halevt_exec *halevt_new_exec(const xmlChar *exec)
    p = parsed_string;
    result_exec->elements[0].string = parsed_string;
    result_exec->elements[0].hal_property = 0;
+
    while ((p = strstr(p, halevt_hal_exec_prefix)) != NULL)
    {
       char *p_begin = p;
@@ -117,10 +176,19 @@ halevt_exec *halevt_new_exec(const xmlChar *exec)
       if ((p = strstr(p, "$")) != NULL)
       {
          sub_elements_number++;
-         result_exec->elements[sub_elements_number].string = p_begin+strlen(halevt_hal_exec_prefix);
-         result_exec->elements[sub_elements_number].hal_property = 1;
          *p = '\0';
          *p_begin = '\0';
+         halevt_parse_property_name(p_begin+strlen("$"), &(result_exec->elements[sub_elements_number].property));
+         if (result_exec->elements[sub_elements_number].property.name == NULL)
+         {
+           free (string);
+           free (parsed_string);
+           free (result_exec);
+           return NULL;
+         }
+         /* result_exec->elements[sub_elements_number].string = p_begin+strlen(halevt_hal_exec_prefix); */
+         result_exec->elements[sub_elements_number].hal_property = 1;
+         result_exec->elements[sub_elements_number].string = NULL;
       }
       else
       {
@@ -133,9 +201,11 @@ halevt_exec *halevt_new_exec(const xmlChar *exec)
       p = p+1;
       sub_elements_number++;
       result_exec->elements[sub_elements_number].string = p;
+      result_exec->elements[sub_elements_number].property.name = NULL;
       result_exec->elements[sub_elements_number].hal_property = 0;
    }
-   result_exec->elements[sub_elements_number+1].string = NULL;
+   /*result_exec->elements[sub_elements_number+1] = NULL;*/
+   result_exec->exec_size = sub_elements_number+1;
    result_exec->string = string;
    result_exec->parsed_string = parsed_string;
    return result_exec;
@@ -143,10 +213,43 @@ halevt_exec *halevt_new_exec(const xmlChar *exec)
 
 static void halevt_free_exec(halevt_exec *exec)
 {
+   int i;
    free(exec->string);
    free(exec->parsed_string);
+   for (i = 0; i < exec->exec_size; i++)
+   {
+     if (exec->elements[i].hal_property == 1)
+     {
+       free (exec->elements[i].property.parents);
+     }
+   }
    free(exec->elements);
    free(exec);
+}
+
+/* for debugging */
+char *halevt_print_property_name(halevt_property_name *property)
+{
+   int total_size = 0;
+   char *string;
+   char **parent;
+   if (property->name == NULL)
+     return NULL;
+   total_size += strlen(property->name)+1;
+   WALK_NULL_ARRAY(parent, property->parents)
+   {
+     total_size += strlen(*parent) +2;
+   }
+   if ((string = (char *) malloc(total_size * sizeof(char))) == NULL)
+      return NULL;
+   *string = '\0';
+   WALK_NULL_ARRAY(parent, property->parents)
+   {
+     strcat(string, *parent);
+     strcat(string, "->");
+   }
+   strcat(string, property->name);
+   return string;
 }
 
 /* for debugging */
@@ -155,28 +258,47 @@ char *halevt_print_exec(halevt_exec *exec_str)
    int i;
    int str_size = 0;
    char *result;
-   for(i = 0; exec_str->elements[i].string != NULL; i++)
+   /* used to store the formatted property strings */
+   char **property_strings;
+
+   if ((property_strings = (char **) malloc(exec_str->exec_size * sizeof (char*))) == NULL)
+     return NULL;
+   
+   for(i = 0; i < exec_str->exec_size; i++)
    {
-      str_size += strlen(exec_str->elements[i].string) + 7;
+      if (exec_str->elements[i].hal_property)
+      {
+         property_strings[i] = halevt_print_property_name (&(exec_str->elements[i].property));
+         if (property_strings[i] != NULL)
+           str_size += strlen(property_strings[i]);
+      }
+      else
+      {
+        str_size += strlen(exec_str->elements[i].string);
+        /* not useful, but cleaner */
+        property_strings[i] = NULL;
+      }
+      str_size += 2;
    }
    if ((result = (char *) malloc((str_size+1)*sizeof(char))) == NULL)
    {
        return NULL;
    }
    *result = '\0';
-   for(i = 0; exec_str->elements[i].string != NULL; i++)
+   for(i = 0; i < exec_str->exec_size; i++)
    {
-      if (exec_str->elements[i].hal_property)
+      if (exec_str->elements[i].hal_property && property_strings[i] != NULL)
       {
-         strcat (result, "1: '");
+         strcat (result, property_strings[i]);
+         free (property_strings[i]);
       }
       else
       {
-         strcat (result, "0: '");
+         strcat (result, exec_str->elements[i].string);
       }
-      strcat(result, exec_str->elements[i].string);
-      strcat(result, "', ");
+      strcat(result, "| ");
    }
+   free (property_strings);
    return result;
 }
 
@@ -197,9 +319,11 @@ halevt_insertion *halevt_add_insertion(const xmlChar *match, const xmlChar *exec
          free(new_insertion);
          return NULL;
       }
+
 /*
       printf ("add_insertion %s, %s\n", match, exec);
 */
+
       new_insertion->next = halevt_insertion_root;
       halevt_insertion_root = new_insertion;
    }
@@ -465,7 +589,9 @@ int halevt_parse_config (char const *path)
                   }
                   else
                   {
-                     halevt_add_insertion (match, exec);
+                     halevt_insertion *new_insertion = halevt_add_insertion (match, exec);
+                     if (new_insertion == NULL)
+                       DEBUG(_("Warning: match %s, exec %s: adding insertion failed"), match, exec);
                      xmlFree(exec);
                   }
                }
@@ -580,7 +706,9 @@ void halevt_print_config()
    insertion = halevt_insertion_root;
    while (insertion != NULL)
    {
-      fprintf(stderr, "insertion %s, %s\n", halevt_print_boolean_expression(insertion->match), halevt_print_exec(insertion->exec));
+      char *exec_string = halevt_print_exec(insertion->exec);
+      fprintf(stderr, "insertion %s, %s\n", halevt_print_boolean_expression(insertion->match), exec_string);
+      free(exec_string);
       insertion = insertion->next;
    }
    removal = halevt_removal_root;

src/parse_config.h
@@ -25,6 +25,7 @@
 typedef struct halevt_exec_element
 {
    char *string;
+   halevt_property_name property;
    int hal_property;
 } halevt_exec_element;
 
@@ -33,6 +34,7 @@ typedef struct halevt_exec
    char *string;
    char *parsed_string;
    halevt_exec_element *elements;
+   int exec_size;
 } halevt_exec;
 
 typedef struct halevt_property_action
@@ -84,6 +86,8 @@ int halevt_parse_config(char const *path);
 void halevt_free_config();
 char *halevt_hal_string (char *string);
 void halevt_print_config ();
+void halevt_parse_property_name (char *string, halevt_property_name *property_name);
+char *halevt_print_property_name(halevt_property_name *property);
 
 extern halevt_property *halevt_property_root;
 extern halevt_insertion *halevt_insertion_root;

commit 091cfa505757e9ecafc3fc885b2bc26e49cff7b3

Really iterate over the str_list
regression inserted in 72822c382d3ef426fc7ecf1104556066b5ff63f3

src/hal_interface.c
@@ -91,8 +91,8 @@ static char **halevt_duplicate_str_list(char** str_list)
                return NULL;
            }
            *cur_value = cur_val;
-           /* already done above
            cur_value++;
+           /* already done above
            *cur_value = NULL;
            */
        }